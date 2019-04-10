d6084fd3ccd23174635dc480b14996caa530ceae
Rose Stough <psithurise@programmer.net>
2019-04-10 20:02:41 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,26 @@ const codes = [
   "a"
 ];
 
+
+
 function init() {
   // your code here
+  //attaching event listeners to keydown events in correct order
+  //of konami code above
+  let index = 0;
+
+  function onKeyDownHandler(e) {
+    const key = e.key
+    if(key === codes[index]) {
+      index++;
+      if(index === codes.length) {
+        alert("Hurray!");
+        index = 0
+      }
+    }
+    else
+        index = 0
+  })
 }
+
+init();
